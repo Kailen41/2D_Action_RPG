29883ce284920aa8d422f796f833dd51a9aea893
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-07-26 22:30:30 -0700

    Initial assets and state machine setup
    -Added assets to the project.
    -Created the needed scripts for the player state machine.
    -Created 2 player states. Idle and move.
    -Created animations for the player idle and move states.
    -Created functionality in the player and player state scripts.

---
+++ Assets/Scripts/Player State Machine/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Components
+    public Animator anim { get; private set; }
+    #endregion
+
+    #region States
+    public PlayerStateMachine stateMachine { get; private set; }
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
+    #endregion
+
+    private void Awake()
+    {
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(stateMachine, this, "Idle");
+        moveState = new PlayerMoveState(stateMachine, this, "Move");
+    }
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+}
