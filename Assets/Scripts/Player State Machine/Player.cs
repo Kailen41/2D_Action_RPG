45cecb64568eb78e958c598d6ea19332afc3138d
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-07-26 22:48:19 -0700

    Movement with inputs
    -Character can now move based on player input.
    -Rigidbody2D velocity used to move the player. Currently, only x axis is setup for player movement.

--- Assets/Scripts/Player State Machine/Player.cs
+++ Assets/Scripts/Player State Machine/Player.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move Info")]
+    public float moveSpeed = 12f;
+
     #region Components
     public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
     #endregion
 
     #region States
@@ -24,6 +28,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
 
         stateMachine.Initialize(idleState);
@@ -32,5 +37,10 @@ public class Player : MonoBehaviour
     private void Update()
     {
         stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
     }
 }
